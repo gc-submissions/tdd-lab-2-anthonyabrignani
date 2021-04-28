16df4586029296b42dc930b2bb616fa51169683c
anthony abrignani <anthony abrignani>
2021-04-28 15:58:53 -0400

    forgot to make branches, gonna try now, isPaymentSufficient done

--- src/js/ChangeHandler.js
+++ src/js/ChangeHandler.js
@@ -1,39 +1,46 @@
-/**
- * This class handles change for a vending machine.
- * 
- * IMPORTANT: All amounts are in cents. E.g. $1.35 = 135. This will help with rounding errors.
- */
 class ChangeHandler {
-    
-    constructor(amountDue) {
-        this.amountDue = null;
-        this.cashTendered = null;
-    }
+  constructor(amountDue) {
+    this.amountDue = amountDue;
+    this.cashTendered = 0;
+  }
 
-    /**
-     * The customer inserts a coin, increasing the cashTendered.
-     * @param {string} type either quarter, dime, nickel, or penny
-     */
-    insertCoin(type) {
-      // TODO
+  insertCoin(type) {
+    const quarter = 25;
+    const dime = 10;
+    const nickel = 5;
+    const penny = 1;
+    let coin = type;
+    if (coin === quarter) {
+      this.cashTendered += quarter;
     }
+    if (coin === dime) {
+      this.cashTendered += dime;
+    }
+    if (coin === nickel) {
+      this.cashTendered += nickel;
+    }
+    if (coin === penny) {
+      this.cashTendered += penny;
+    }
+    return this.cashTendered;
+  }
 
-    /**
-     * Returns true if enough coins have been inserted to at least meet the amountDue
-     */
-    isPaymentSufficient() {
-      // TODO
+  isPaymentSufficient() {
+    if (this.amountDue <= this.cashTendered) {
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    giveChange() {
-        // TODO return the correct change in the following format...
-        return {
-            quarters: 0,
-            dimes: 0,
-            nickels: 0,
-            pennies: 0
-        }
-    }
+  giveChange() {
+    return {
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0,
+    };
+  }
 }
 
 module.exports = ChangeHandler;
--- test/ChangeHandler.test.js
+++ test/ChangeHandler.test.js
@@ -1,12 +1,71 @@
 const ChangeHandler = require("../src/js/ChangeHandler");
+const quarter = 25;
+const dime = 10;
+const nickel = 5;
+const penny = 1;
 
-describe("ChangeHandler", function() {
-  test("class is defined", function() {
-    // Remember, you can arrange, act, and assert...start small
+describe("ChangeHandler", function () {
+  test("class is defined", function () {
     expect(ChangeHandler).toBeDefined();
   });
 
-  // TODO: Add additional tests below...
+  test("cashTendered is 0", function () {
+    const cash = new ChangeHandler();
+    expect(cash.cashTendered).toEqual(0);
+  });
 
+  test("insertCoin - quarter is 25", function () {
+    const cash = new ChangeHandler();
+    cash.insertCoin(quarter);
+    expect(cash.cashTendered).toEqual(25);
+  });
 
+  test("insertCoin - dime is 10", function () {
+    const cash = new ChangeHandler();
+    cash.insertCoin(dime);
+    expect(cash.cashTendered).toEqual(10);
+  });
+
+  test("insertCoin - dime is 5", function () {
+    const cash = new ChangeHandler();
+    cash.insertCoin(nickel);
+    expect(cash.cashTendered).toEqual(5);
+  });
+
+  test("insertCoin - penny is 1", function () {
+    const cash = new ChangeHandler();
+    cash.insertCoin(penny);
+    expect(cash.cashTendered).toEqual(1);
+  });
+
+  test("insertCoin - multiple coins add to total", function () {
+    const cash = new ChangeHandler();
+    cash.insertCoin(quarter);
+    cash.insertCoin(quarter);
+    cash.insertCoin(dime);
+    cash.insertCoin(nickel);
+    cash.insertCoin(penny);
+    expect(cash.cashTendered).toEqual(66);
+  });
+
+  test("isPaymentSufficient - true if tendered > amountDue", function () {
+    const cash = new ChangeHandler(25);
+    cash.insertCoin(quarter);
+    cash.insertCoin(quarter);
+    expect(cash.isPaymentSufficient()).toEqual(true);
+  });
+
+  test("isPaymentSufficient - false if tendered < amountDue", function () {
+    const cash = new ChangeHandler(75);
+    cash.insertCoin(quarter);
+    cash.insertCoin(quarter);
+    expect(cash.isPaymentSufficient()).toEqual(false);
+  });
+
+  test("isPaymentSufficient - true if tendered = amountDue", function () {
+    const cash = new ChangeHandler(50);
+    cash.insertCoin(quarter);
+    cash.insertCoin(quarter);
+    expect(cash.isPaymentSufficient()).toEqual(true);
+  });
 });
